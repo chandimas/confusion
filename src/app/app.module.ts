a6d4ea4a5c2074e241108f8d0e2684cbf2a32474
Chandima Silva <chandimasilva1@gmail.com>
2018-09-20 22:52:31 +0530

    menu dish details added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,17 +10,28 @@ import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 import {MatListModule} from '@angular/material/list';
 
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import { DishdetailsComponent } from './dishdetails/dishdetails.component';
+
+
+
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    DishdetailsComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatToolbarModule,
     FlexLayoutModule,
-    MatListModule
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
